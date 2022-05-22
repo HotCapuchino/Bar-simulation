d3ca243eb7da57cad952c66b3547b5fab4f68d11
Andrew <and.razumov2014@gmail.com>
2022-05-23 01:30:29 +0800

    added config loader

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+import sys
 from time import sleep
+# Использование библиотеки на линукс требует рута -> sudo pip3 install keyboard, запускать скрипт от sudo
 import keyboard
+import json
 
 
-iteration_len = 1
+def load_config(file_path):
+    try:
+        file = open(file_path)
+        config = json.load(file)
+        file.close()
+        return config
+    except Exception:
+        print("Error while reading json file!")
+        sys.exit(-1)
+
+GENERAL_CONFIG_FILE_PATH = 'resources/config.json'
+ARTISTS_CONFIG_FILE_PATH = 'resources/artists.json'
+MACRO_CONFIG_FILE_PATH = 'resources/macro.json'
+
+GENERAL_CONFIG = load_config(GENERAL_CONFIG_FILE_PATH)
+ARTISTS_CONFIG = load_config(ARTISTS_CONFIG_FILE_PATH)
+MACRO_CONFIG = load_config(MACRO_CONFIG_FILE_PATH)
+
+ITERATION_LEN = GENERAL_CONFIG['iteration_len']
 
 income = []
 expenses = []
@@ -12,7 +33,7 @@ while True:
     # здесь должны вызываться какие-то функции
 
     # на основе результатов заполняются ячейки в массивах доходов и расходов
-    sleep(iteration_len)
+    sleep(ITERATION_LEN)
 
     if keyboard.is_pressed('q'):
         break
